b9e10175429f372e60cf9b45d878cf52837a7e2a
katarzynakich <katarzynakich@gmail.com>
2017-05-28 01:06:41 +0200

    grunt-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,25 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'style.css': 'style.sass'
+  			}
+  		}
+  	},
     jshint: {
       all: ['js/*.js']
     }
   });
   // Load the plugins tasks
+   grunt.loadNpmTasks('grunt-sass');
    grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
 
-   grunt.registerTask('default', ['jshint']);
+   grunt.registerTask('default', ['sass', 'jshint']);
 };
